01263f15d082bf912cbd607f0f3608f17a9d94b6
Site Huang <huangsite378@gmail.com>
2019-11-12 09:32:24 +1100

    Change Proxy Setting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const proxy = require("http-proxy-middleware");
+const port = process.env.PORT || 5000;
 
 module.exports = function(app) {
-  app.use(proxy("/auth/**", { target: "http://localhost:5000" }));
-  app.use(proxy("/order", { target: "http://localhost:5000" }));
+  app.use(proxy("/auth/**", { target: `http://localhost:${port}` }));
+  app.use(proxy("/order", { target: `http://localhost:${port}` }));
 };
